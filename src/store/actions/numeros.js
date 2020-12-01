ebc46c8dd28ec081a1c58983bc982458cf278f40
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-01 18:56:16 +0100

    using constants to  name types of reducers

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,7 +1,7 @@
-// Action Creator
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from "./actionTypes";
 export const alterarNumeroMinimo = (novoNumero) => {
   return {
-    type: "NUM_MIN_ALTERADO",
+    type: NUM_MIN_ALTERADO,
     payload: novoNumero,
   };
 };
@@ -9,7 +9,7 @@ export const alterarNumeroMinimo = (novoNumero) => {
 // Action Creator
 export const alterarNumeroMaximo = (novoNumero) => {
   return {
-    type: "NUM_MAX_ALTERADO",
+    type: NUM_MAX_ALTERADO,
     payload: novoNumero,
   };
 };
--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,3 +1,5 @@
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from "../actions/actionTypes";
+
 const initialState = {
   min: 1,
   max: 10,
@@ -6,13 +8,13 @@ const initialState = {
 const numeroReducer = (state = initialState, action) => {
   // se n for passado estado, o padrão é initialState
   switch (action.type) {
-    case "NUM_MIN_ALTERADO": {
+    case NUM_MIN_ALTERADO: {
       return {
         ...state,
         min: action.payload,
       };
     }
-    case "NUM_MAX_ALTERADO": {
+    case NUM_MAX_ALTERADO: {
       return {
         ...state,
         max: action.payload,
